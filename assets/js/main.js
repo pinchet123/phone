96ff2db11975461f05f2a2de3ce754e898f403db
pinchet123 <ngojack61@gmail.com>
2020-08-01 23:27:43 +0700

    game ban ca

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,14 @@ setTime = setInterval(function(){
     var h = day.getHours();
     var m = day.getMinutes();
     var s = day.getSeconds();
-    var time = `${h} : ${m} : ${s}`;
+    var time = `${h}:${m}:${s}`;
     document.getElementById('time').innerText = time;
 })
+
+    var banca = `<iframe src="iframe/fish/index.html" frameborder="0"></iframe>`
+
+    $('#banca').click(function(){
+        $('.out').css('display' , 'block')
+        $('div.main_item').css('height' , '100%');
+        $('.main').html(banca)
+    }) 
