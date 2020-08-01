0608febbca18678a8865c51cb9478300f63c0e06
pinchet123 <ngojack61@gmail.com>
2020-08-01 23:47:22 +0700

    lam nut out game

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var setTime = null;
-setTime = setInterval(function(){
+setTime = setInterval(function() {
     var day = new Date();
     var h = day.getHours();
     var m = day.getMinutes();
@@ -7,11 +7,55 @@ setTime = setInterval(function(){
     var time = `${h}:${m}:${s}`;
     document.getElementById('time').innerText = time;
 })
+var game = ` <div class="main_item">
+<div class="wrap-main">
+    <div class="main_item-game" id="banca">
+        <p>game bắn cá</p>
+        <div class="game">
+            <img src="assets/img/game/banca.jpg" alt="">
+            
+        </div>
+        
+    </div>
+    <div class="main_item-game">
+        <p>game bắn cá</p>
+        <div class="game">
+            <img src="assets/img/game/banca.jpg" alt="">
+        </div>
+        
+    </div>
+    <div class="main_item-game">
+        <p>game bắn cá</p>
+        <div class="game">
+            <img src="assets/img/game/banca.jpg" alt="">
+        </div>
+        
+    </div>
+    <div class="main_item-game">
+        <p>game bắn cá</p>
+        <div class="game">
+            <img src="assets/img/game/banca.jpg" alt="">
+        </div>
+        
+    </div>
+   
+</div>
 
-    var banca = `<iframe src="iframe/fish/index.html" frameborder="0"></iframe>`
 
-    $('#banca').click(function(){
-        $('.out').css('display' , 'block')
-        $('div.main_item').css('height' , '100%');
-        $('.main').html(banca)
-    }) 
+</div>`
+var banca = `<iframe src="iframe/fish/index.html" frameborder="0"></iframe>`
+
+// $('#banca').click(function() {
+//     $('.out').css('display', 'block')
+//     $('div.main_item').css('height', '100%');
+//     $('.main').html(banca)
+// })
+$(document).on('click', '#banca', function() {
+    $('.out').css('display', 'block')
+    $('div.main_item').css('height', '100%');
+    $('.main').html(banca)
+})
+$('.out').click(function() {
+    $('iframe').remove()
+    $('.main').html(game)
+})
