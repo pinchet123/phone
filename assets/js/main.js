37641004de2976ecfef9c20a913764d0bd7a0c93
pinchet123 <ngojack61@gmail.com>
2020-08-02 19:21:11 +0700

    phone

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,13 +10,13 @@ setTime = setInterval(function() {
     // clock
 var new_game = {
         "fish": [
-            { "name": "fish", "image": 'banca.png' , 'title' : 'Bắn cá'},
+            { "name": "fish", "image": 'banca.png', 'title': 'Bắn cá' },
         ],
         "caro": [
-            { "name": "caro", "image": 'caro.png' , 'title' : 'Cờ caro'},
+            { "name": "caro", "image": 'caro.png', 'title': 'Cờ caro' },
         ],
         "bird": [
-            { "name": "bird", "image": 'bird.png' , 'title' : 'Flappy Bird'},
+            { "name": "bird", "image": 'bird.png', 'title': 'Flappy Bird' },
         ]
     }
     // obj game 
@@ -37,23 +37,21 @@ function get_game() {
         for (var i = 0; i < game_array.length; i++) {
             var obj = game_array[i];
             main_item_game = `
-           
-                    <div class="main_item-game" id="banca" data="${game_array[i].name}">
-                        <p>${game_array[i].title}</p>
-                        <div class="game">
-                            <img src="assets/img/game/${game_array[i].image}" alt="">
+                <div class="main_item-game" id="banca" data="${game_array[i].name}">
+                    <p>${game_array[i].title}</p>
+                    <div class="game">
+                    <img src="assets/img/game/${game_array[i].image}" alt="">
                     </div>
-              
-        
+                </div>
            `
             $('.wrap-main').append(main_item_game)
-
 
         }
 
     }
 }
-get_game()
+// get_game()
+
 $(document).on('click', '.main_item-game', function() {
     var game = $(this).attr('data');
     var iframe = `<iframe src="iframe/${game}/index.html" frameborder="0"></iframe>`
@@ -70,3 +68,36 @@ $('.out').click(function() {
     $(this).css('display', 'none')
     get_game()
 })
+
+
+var data = [];
+var input = $('input')
+var number = ""
+
+function call() {
+    var num = $('p.num_value');
+    num.click(function() {
+        data.push($(this).text())
+        number = data.join('')
+        input.val(number)
+        console.log(number)
+
+
+    })
+    $('#delete_number').click(function() {
+
+
+        if (data.length > 0) {
+            number = number.split('')
+            console.log(typeof number)
+            number = data.pop()
+            var num = data.join('')
+        }
+
+        input.val(num)
+
+
+    })
+
+}
+call()
